b8080c48d236122533b6a46c7ee962997bedbc2e
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2022-01-08 23:34:26 -0500

    finished project for now. animations all are good. styling could be better but not important. animations understood much better in form context.

--- app.js
+++ app.js
@@ -109,3 +109,70 @@ function colorize(color, line, placeholder){
     gsap.to(line, {stroke: color, duration: 0.75, ease: "Power2.easeOut"});
     gsap.to(placeholder, {color: color, duration: 0.75, ease: "Power2.easeOut"});
 }
+
+//! Checkbox Animation Fill
+const checkbox = document.querySelector(".checkbox");
+const tl2 = gsap.timeline({ defaults: { duration: 0.5, ease: 'Power2.easeOut' } });
+const tickMarkPath = document.querySelector('.tick-mark path');
+const pathLength = tickMarkPath.getTotalLength();
+
+gsap.set(tickMarkPath, 
+    {
+        strokeDashoffset: pathLength, 
+        strokeDasharray: pathLength
+    }
+);
+
+checkbox.addEventListener('click', () => {
+    if(checkbox.checked) {
+        tl2.to('.checkbox-fill', { top:  '0%'});
+        tl2.fromTo(tickMarkPath, { 
+            strokeDashoffset: pathLength }, { 
+                strokeDashoffset: 0, 
+                duration: 0.5, 
+                ease: 'Power2.easeOut' },
+                '<50%'
+                );
+        tl.to('.checkbox-label', { color: '#6391E8' }, '<');
+    }
+    else {
+        tl2.to('.checkbox-fill', { top: '100%'});
+        tl2.fromTo(tickMarkPath, { 
+            strokeDashoffset: 0 }, { 
+                strokeDashoffset: pathLength, 
+                duration: 0.5, 
+                ease: 'Power2.easeOut' },
+                '<50%'
+                );
+                tl2.to('.checkbox-label', { color: '#c5c5c5' }, '<');
+    }
+});
+
+//! Character animation
+gsap.set('#eye', { transformOrigin: '50% 50%' });
+gsap.fromTo('#eye', {scaleY: 1}, {scaleY: 0.3, repeat: -1, yoyo: 'true', repeatdelay: 0.5, ease: "Power2.easeOut"});
+gsap.fromTo('#eyebrow', {y: 0}, {y: -1, yoyo: true, repeat: -1, repeatDelay: 0.5, ease: "Power2.easeOut"});
+
+//! Submit Button Animation
+const button = document.querySelector('button');
+const tl3 = gsap.timeline({ 
+    defaults: { duration: 0.75, ease: 'Power2.easeOut' } 
+});
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    tl3.to(".contact-right, .contact-left", {
+      y: 30,
+      opacity: 0,
+      pointerEvents: "none",
+    });
+    tl3.to("form", { scale: 0.8 }, "<");
+    tl3.fromTo(".submitted", { opacity: 0, y: 30 }, { opacity: 1, y: 0 });
+    //Hand wave
+    gsap.set("#hand", { transformOrigin: "left" });
+    gsap.fromTo(
+      "#hand",
+      { rotation: 0, y: 0 },
+      { rotation: -10, y: 2, ease: "elastic(3,0.3)", duration: 2, delay: 1 }
+    );
+  });
